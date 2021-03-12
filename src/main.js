efdfd0707781f1f62e64174e5d14ee85e39a908e
shanole <73411129+shanole@users.noreply.github.com>
2021-03-11 22:27:12 -0800

    Basic jQuery to make form work

--- src/main.js
+++ src/main.js
@@ -2,3 +2,26 @@ import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
+import Person from './js/person.js';
+
+$("form#calculator").submit(function(event) {
+  event.preventDefault();
+  const user = new Person(parseInt($("#age").val()), $("#sex").val());
+  const userName = $("#name").val();
+  const selectedPlanet = $("#planet").val();
+  let userHabits =[];
+
+  $("input:checkbox:checked").each(function (){
+    userHabits.push($(this).val);
+  });
+  user.setAdjustedLifeExpectancy(userHabits);
+  const timeLeft = user.getTimeLeft(selectedPlanet);
+
+  $("#user-name").text(userName);
+  $("#your-planet").text(selectedPlanet);
+  $("#planet-age").text(user.getPlanetAge(selectedPlanet));
+  $("#time-left").text(timeLeft);
+
+  $("#age-results").show();
+
+});
