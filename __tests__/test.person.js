6ec1dc7f407fd6e058365543472e7301d8aba25f
shanole <73411129+shanole@users.noreply.github.com>
2021-03-11 18:32:14 -0800

    Refactor test file to have beforeEach method and also pass test for hhabits property of a new Person object

--- __tests__/test.person.js
+++ __tests__/test.person.js
@@ -1,10 +1,17 @@
-import { TestScheduler } from 'jest'
 import Person from './../src/js/person.js'
 
 describe('Person', () => {
+  let testPerson;
+  beforeEach(() => {
+    testPerson = new Person(25,"female")
+  });
+
   test('should create a Person object with age and sex properties', () => {
-    let testPerson = new Person(25,"female");
     expect(testPerson.age).toEqual(25);
     expect(testPerson.sex).toEqual("female");
+  })
+
+  test('should have a habits attribute that is an array', () => {
+    expect(testPerson.habits).toEqual([]);
   })
 })
--- src/js/person.js
+++ src/js/person.js
@@ -2,5 +2,6 @@ export default class Person {
   constructor(age,sex) {
     this.age = age;
     this.sex = sex;
+    this.habits = [];
   }
 }
