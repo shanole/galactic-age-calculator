fa8831cd00fdd527feddd5ebccf2d294e92203d4
shanole <73411129+shanole@users.noreply.github.com>
2021-03-18 15:22:51 -0700

    Change test specs to be more readable

--- __tests__/test.person.js
+++ __tests__/test.person.js
@@ -24,30 +24,30 @@ describe('Person', () => {
   });
 
   test("should have a getPlanetAge method that returns the Person's age divided by 0.24 when the given parameter is Mercury", () => {
-    expect(testPerson.getPlanetAge("Mercury")).toEqual(25/.24);;
+    expect(testPerson.getPlanetAge("Mercury")).toEqual("104.17");;
   });
 
   test("should have a getPlanetAge method that returns the Person's age divided by 0.62 when the given parameter is Venus", () => {
-    expect(testPerson.getPlanetAge("Venus")).toEqual(25/.62);
+    expect(testPerson.getPlanetAge("Venus")).toEqual("40.32");
   });
 
   test("should have a getPlanetAge method that returns the Person's age divided by 1.88 when the given parameter is Mars", () => {
-    expect(testPerson.getPlanetAge("Mars")).toEqual(25/1.88);
+    expect(testPerson.getPlanetAge("Mars")).toEqual("13.30");
   });
 
   test("should have a getPlanetAge method that returns the Person's age divided by 11.86 when the given parameter is Jupiter", () => {
-    expect(testPerson.getPlanetAge("Jupiter")).toEqual(25/11.86);
+    expect(testPerson.getPlanetAge("Jupiter")).toEqual("2.11");
   });
 
   test("should have a method getTimeLeft that returns the difference between the adjusted life expectancy on a given planet and the Person's age on that planet", () => {
-    expect(testPerson.getTimeLeft("Mercury")).toEqual((81/0.24) - (25/0.24));
-    expect(testPerson.getTimeLeft("Venus")).toEqual((81/0.62) - (25/0.62));
-    expect(testPerson.getTimeLeft("Mars")).toEqual((81/1.88) - (25/1.88));
-    expect(testPerson.getTimeLeft("Jupiter")).toEqual((81/11.86) - (25/11.86));
+    expect(testPerson.getTimeLeft("Mercury")).toEqual("233.33");
+    expect(testPerson.getTimeLeft("Venus")).toEqual("90.33");
+    expect(testPerson.getTimeLeft("Mars")).toEqual("29.79");
+    expect(testPerson.getTimeLeft("Jupiter")).toEqual("4.72");
   });
 
   test('should have a getTimeLeft method that returns a negative number if the Person has exceeded the average life expectancy', () => {
     let oldPerson = new Person(90,"female");
-    expect(oldPerson.getTimeLeft("Mercury")).toEqual(-37.5);
+    expect(oldPerson.getTimeLeft("Mercury")).toEqual("-37.50");
   });
 })
